7101f25e2a965ea4b8004e7c4694466bb5d74fa1
lucianodinaso <luchoargentina@hotmail.com>
2023-04-30 17:45:12 -0300

    Create Vista_EstadoResultado.sql

---
+++ Vista_EstadoResultado.sql
@@ -0,0 +1,27 @@
+create view v_EstadoResultado as
+select month(v.fecha_venta) as Mes,
+sum(v.precio_venta) as Venta,
+sum(c.costo_reposicion ) as Costo_Compra,
+round(sum(v.precio_venta)*0.09)  as Envio,
+round(sum(v.precio_venta)*0.15) as Comision,
+#Calculo de utilidad
+round(
+sum(v.precio_venta) -
+sum(c.costo_reposicion ) -
+round(sum(v.precio_venta)*0.09) -
+round(sum(v.precio_venta)*0.15) 
+) as Utilidad,
+#Calculo de Margen = (Utilidad - Ventas)*100
+round(((round(
+sum(v.precio_venta) -
+sum(c.costo_reposicion ) -
+round(sum(v.precio_venta)*0.09) -
+round(sum(v.precio_venta)*0.15) 
+))/
+sum(v.precio_venta))
+*100,2)
+as Margen
+from ventas as v
+inner join costos as c
+on v.Id_costos = c.Id_costos
+group by month(v.fecha_venta);
